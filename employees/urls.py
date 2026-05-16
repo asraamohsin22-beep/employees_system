10c160d911bf18db14207d3dd0ba04ae285f52cf
asraa <asraamohsin22@gmail.com>
2026-05-17 01:15:13 +0300

    Fix upload permissions

--- employees/urls.py
+++ employees/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.login_view, name='login'),  # 👈 هذا الحل
     path('salary/', views.salary_lookup, name='salary_lookup'),
     path('logout/', views.logout_view, name='logout'),
-    path('pdf/<str:emp_id>/', views.generate_pdf, name='generate_pdf'),
+   # path('pdf/<str:emp_id>/', views.generate_pdf, name='generate_pdf'),
     path('upload/', views.upload_excel, name='upload_excel'),
+    
 ]
